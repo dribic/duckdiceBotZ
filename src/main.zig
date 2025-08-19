d16dc695f0180e7b298cc1b1cddd4fda9caa2a88
Dejan Ribič <dejan.ribic@gmail.com>
2025-08-19 10:08:51 +0200

    Split structs into a seperate file.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,50 +1,5 @@
 const std = @import("std");
-
-pub const Response = struct {
-    hash: ?[]const u8,
-    username: ?[]const u8,
-    createdAt: ?i64,
-    level: ?i64,
-    campaign: ?[]const u8,
-    affiliate: ?[]const u8,
-    lastDeposit: ?LastDeposit,
-    wagered: ?[]const WageredItem,
-    balances: ?[]const BalanceItem,
-    wageringBonuses: ?[]const WageringBonus,
-    tle: ?[]const TleItem,
-};
-
-pub const LastDeposit = struct {
-    createdAt: ?i64,
-    currency: ?[]const u8,
-    amount: ?[]const u8,
-};
-
-pub const WageredItem = struct {
-    currency: ?[]const u8,
-    amount: ?[]const u8,
-};
-
-pub const BalanceItem = struct {
-    currency: ?[]const u8,
-    main: ?[]const u8,
-    faucet: ?[]const u8 = null,
-};
-
-pub const WageringBonus = struct {
-    name: ?[]const u8,
-    type: ?[]const u8,
-    hash: ?[]const u8,
-    status: ?[]const u8,
-    symbol: ?[]const u8,
-    margin: ?[]const u8,
-};
-
-pub const TleItem = struct {
-    hash: ?[]const u8,
-    name: ?[]const u8,
-    status: ?[]const u8,
-};
+const types = @import("types.zig");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -77,7 +32,7 @@ pub fn main() !void {
 
     const response_body = try get(url, headers, &client, allocator);
 
-    var result = try std.json.parseFromSlice(Response, allocator, response_body.items, .{ .ignore_unknown_fields = true });
+    var result = try std.json.parseFromSlice(types.Response, allocator, response_body.items, .{ .ignore_unknown_fields = true });
 
     defer result.deinit();
 
