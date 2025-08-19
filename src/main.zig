f422aefe878ac82f7a9377a706915ac50556fe18
Dejan Ribič <dejan.ribic@gmail.com>
2025-08-19 10:03:08 +0200

    Starting development.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,132 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("ddBot_lib");
+pub const Response = struct {
+    hash: ?[]const u8,
+    username: ?[]const u8,
+    createdAt: ?i64,
+    level: ?i64,
+    campaign: ?[]const u8,
+    affiliate: ?[]const u8,
+    lastDeposit: ?LastDeposit,
+    wagered: ?[]const WageredItem,
+    balances: ?[]const BalanceItem,
+    wageringBonuses: ?[]const WageringBonus,
+    tle: ?[]const TleItem,
+};
+
+pub const LastDeposit = struct {
+    createdAt: ?i64,
+    currency: ?[]const u8,
+    amount: ?[]const u8,
+};
+
+pub const WageredItem = struct {
+    currency: ?[]const u8,
+    amount: ?[]const u8,
+};
+
+pub const BalanceItem = struct {
+    currency: ?[]const u8,
+    main: ?[]const u8,
+    faucet: ?[]const u8 = null,
+};
+
+pub const WageringBonus = struct {
+    name: ?[]const u8,
+    type: ?[]const u8,
+    hash: ?[]const u8,
+    status: ?[]const u8,
+    symbol: ?[]const u8,
+    margin: ?[]const u8,
+};
+
+pub const TleItem = struct {
+    hash: ?[]const u8,
+    name: ?[]const u8,
+    status: ?[]const u8,
+};
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const alloc = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    const allocator = arena.allocator();
+
+    const file = std.fs.cwd().openFile("API.txt", .{ .mode = .read_only }) catch |err| {
+        try stdout.print("API file error: {any}\n", .{err});
+        return;
+    };
+    var reader = std.io.bufferedReader(file.reader());
+    var in_stream = reader.reader();
+    var line_buffer: [124]u8 = undefined;
+
+    const api = try in_stream.readUntilDelimiterOrEof(&line_buffer, '\n');
+
+    var url_buffer: [1022]u8 = undefined;
+    const url = try std.fmt.bufPrint(&url_buffer, "https://duckdice.io/api/bot/user-info?api_key={s}", .{api.?});
+    file.close();
+    defer arena.deinit();
+
+    var client = std.http.Client{
+        .allocator = allocator,
+    };
+
+    const headers = &[_]std.http.Header{
+        .{ .name = "X-Custom-Header", .value = "application" },
+    };
+
+    const response_body = try get(url, headers, &client, allocator);
+
+    var result = try std.json.parseFromSlice(Response, allocator, response_body.items, .{ .ignore_unknown_fields = true });
+
+    defer result.deinit();
+
+    if (result.value.username) |username| {
+        try stdout.print("Parsed user data for: {s}\n", .{username});
+    } else {
+        try stdout.print("User data loaded, but username field was missing.\n", .{});
+    }
+
+    if (result.value.balances) |balances| {
+        try stdout.print("User's balances:\n", .{});
+        for (balances) |balance| {
+            if (balance.currency) |currency| {
+                try stdout.print("  - Currency: {s}\n", .{currency});
+            }
+            if (balance.main) |main_balance| {
+                try stdout.print("    Main balance: {s}\n", .{main_balance});
+            }
+            if (balance.faucet) |faucet_balance| {
+                try stdout.print("    Faucet balance: {s}\n", .{faucet_balance});
+            }
+        }
+    } else {
+        try stdout.print("User data loaded, but the balances field was missing.\n", .{});
+    }
+}
+
+fn get(
+    url: []const u8,
+    headers: []const std.http.Header,
+    client: *std.http.Client,
+    allocator: std.mem.Allocator,
+) !std.ArrayList(u8) {
+    const stdout = std.io.getStdOut().writer();
+    const limit = std.mem.indexOf(u8, url, "=").?;
+    try stdout.print("\nURL: {s}<API-KEY> GET\n", .{url[0 .. limit + 1]});
+
+    var response_body = std.ArrayList(u8).init(allocator);
+
+    try stdout.print("Sending request...\n", .{});
+    const response = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .url = url },
+        .extra_headers = headers, //put these here instead of .headers
+        .response_storage = .{ .dynamic = &response_body }, // this allows us to get a response of unknown size
+    });
+
+    try stdout.print("Response Status: {d}\n Response Body:{s}\n", .{ response.status, response_body.items });
+
+    // Return the response body to the caller
+    return response_body;
+}
